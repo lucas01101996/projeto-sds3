cd2cc9f884bb25820385f1019891c9310945dbcb
Lucas Ferraz <lucas_nogueira155@hotmail.com>
2021-09-08 00:54:44 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/lucas01101996" target="_blank" rel="noreferrer">Lucas Ferraz</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
